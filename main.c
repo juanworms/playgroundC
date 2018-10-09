18b22cc4a6c888986f37aa5f32c2e5bbdaf73e79
Juan B <jbenitez@fi.uba.ar>
2018-10-09 15:54:54 -0300

    remove void, because fun

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-int main (void) {
-<<<<<<< HEAD
+int main () {
     printf("Holi Wordls\n");
     return 0;
-=======
-
-    printf("Holi Wordls\n");
->>>>>>> 2b54cb450219bb5cde95abdac511c67b8627db06
 }
